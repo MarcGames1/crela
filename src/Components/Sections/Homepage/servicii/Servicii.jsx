87d32bce5da753d5c530890f83d7154defd05e6c
MarcGames1 <George.marcu20@gmail.com>
2023-02-23 00:39:14 +0200

    x

--- src/Components/Sections/Homepage/servicii/Servicii.jsx
+++ src/Components/Sections/Homepage/servicii/Servicii.jsx
@@ -12,12 +12,45 @@ export const Servicii = () => {
     <section id="servicii">
       <div className={tw('flex items-center flex-col')}>
         <span className={typography.heading1}>Servicii</span>
-        <div>
+        <div
+          className={tw(
+            'grid grid-cols-3 grid-rows-3 items-center 	  justify-items-center '
+          )}
+        >
+          <span className={`{tw("row-span-1")} ${typography.decorativ}`}>
+            Tu
+          </span>
           <Door
+            style={tw('row-span-1 col-start-2 col-end-2')}
             src={'/doors/copywriting-door.png'}
             subtitlu={'Ai ceva-n minte, dar nu știi s-o exprimi?'}
             titlu={'COPYWRITING'}
           />
+          <span className={`{tw("row-span-1")} ${typography.decorativ}`}>
+            Ce
+          </span>
+          <span
+            className={`{tw(" row-span-2 col-span-1")} ${typography.decorativ}`}
+          >
+            ușă
+          </span>
+          <Door
+            style={tw('row-span-2 col-start-2 col-end-2')}
+            src={'/doors/copywriting-door.png'}
+            subtitlu={'Îți dorești să fii-n atenția tuturor ?'}
+            titlu={'SOCIAL MEDIA'}
+          />
+          <Door
+            style={tw('row-span-3 col-start-2 col-end-2')}
+            src={'/doors/copywriting-door.png'}
+            subtitlu={'Vrei să faci din gândurile tale-o artă?'}
+            titlu={'GRAPHIC DESIGN'}
+          />
+          <span
+            className={`{tw("row-span-3 col-start-3 col-end-3")} ${typography.decorativ}`}
+          >
+            alegi?
+          </span>
         </div>
       </div>
     </section>
@@ -32,10 +65,10 @@ export const Servicii = () => {
 
 
 
-const Door = ({titlu, subtitlu, src}) => {
+const Door = ({titlu, subtitlu, src, style}) => {
   return(
 
-    <div className={tw('flex flex-col items-center')}>
+    <div className={ `${style} ${tw('flex flex-col items-center')}`}>
 
  <Image src={src} width={132} height={175} />
  <span className={typography.heading2}>{titlu}</span>
--- src/helpers/styles.js
+++ src/helpers/styles.js
@@ -24,9 +24,10 @@ export const typography = {
   heading1: `${styles.heading1} ${tw('')}`,
   heading2: `${styles.heading2} ${tw('')}`,
 
+  decorativ: `${styles.decorativ} ${tw('text-[#5e62826e]')}`,
 
-  p:`${tw(
+  p: `${tw(
     'lg:text-xl md:text-sm sm:text-sm leading-18 indent-0	text-justify		'
   )} ${styles.p}`,
-}
+};
   
