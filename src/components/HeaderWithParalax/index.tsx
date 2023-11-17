bc914bf4e1c6dd11dd198d66ac7b9db5a94a6c17
MarcGames1 <George.marcu20@gmail.com>
2023-11-17 11:52:24 +0200

    x

--- src/components/HeaderWithParalax/index.tsx
+++ src/components/HeaderWithParalax/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { PropsWithChildren } from 'react'
-import { Parallax } from 'react-parallax';
-
+import { Parallax, Background } from 'react-parallax';
+import Image from 'next/image';
 
 interface HeaderWithParalaxProps {
   bgImage: string;
@@ -19,7 +19,16 @@ const HeaderWithParalax = ({
       bgImage={bgImage}
       strength={500}
     >
-      <div className="  flex  items-center py-20 text-center overflow-hidden ">
+      <Background className="custom-bg w-full h-full relative">
+        <Image
+          src={bgImage}
+          alt="fill murray"
+          objectFit="contain"
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </Background>
+      <div className="  flex text-white items-center py-20 text-center overflow-hidden ">
         {children}
       </div>
     </Parallax>
--- src/components/ToateServiciile/LiniaRosie.tsx
+++ src/components/ToateServiciile/LiniaRosie.tsx
@@ -2,21 +2,28 @@ import React from 'react'
 import { useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 
-const LiniaRosie = ({y=1}) => {
+const LiniaRosie = ({ y = 55.2 }) => {
+  const lineAnimation = useAnimation();
 
+  useEffect(() => {
+    lineAnimation.start({
+      y,
+      transition: {
+        type: 'linear',
+        duration: 0.1,
+        delay: 0.2,
+      },
+    });
 
-    const lineAnimation = useAnimation()
-
-    useEffect(() =>{
-        lineAnimation.start({y, transition:{
-        type:'linear',
-        duration: .1, 
-         delay: .2,}})
-    },[y])
+    console.log(y);
+  }, [y]);
 
   return (
-    <motion.div animate={lineAnimation} className="absolute left-0 top-0 h-[3px] w-full  bg-[#C61D23] transition-all duration-300 ease-in-out"></motion.div>
+    <motion.div
+      animate={lineAnimation}
+      className="absolute left-0 top-0 h-[3px] w-full  bg-[#C61D23] transition-all duration-300 ease-in-out"
+    ></motion.div>
   );
-}
+};
 
 export default LiniaRosie
