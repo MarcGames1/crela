8465fc909eb744f5eb2d01a56a08b289f75253c7
MarcGames1 <George.marcu20@gmail.com>
2023-11-26 01:35:04 +0200

    xxx

--- src/app/servicii/creare-logo/page.tsx
+++ src/app/servicii/creare-logo/page.tsx
@@ -1,8 +1,9 @@
-import { PageComponent } from '@/components';
+import { PageComponent,HeaderWithParalax } from '@/components';
 import { Metadata } from 'next/types';
 import React from 'react';
 import { IMainHeader } from '@/components/MainHeader/MainHeader';
 import MainHeader from '@/components/MainHeader/MainHeader';
+import Link from 'next/link';
 const MainHeaderData: IMainHeader = {
   title: 'Creare logo personalizat pentru firma ta',
   paragraph:
@@ -27,6 +28,39 @@ const CreareLogo = () => {
   return (
     <PageComponent>
       <MainHeader {...MainHeaderData} />
+      <section>
+        <h2>Ce trebuie sa transmita un logo</h2>
+        [Exemple de la branduri cunoscute cu poze]
+      </section>
+
+      <section>
+        <h2>Uite o parte din logo-urile realizate de mine</h2>
+      </section>
+      <section>
+        <h2> Creare logo Firma Transport - Exemplu</h2>
+      </section>
+      <section>
+        <h2> Creare logo Firma Constructii - Exemplu</h2>
+      </section>
+      <section className="flex flex-col">
+        <HeaderWithParalax bgImage="/images/assets/parallax1.png">
+          <div className='m-auto'>
+            <h2
+              className="mx-auto text-3xl font-bold leading-[1.25] md:text-5xl md:leading-tight 
+                  [&>u]:decoration-[2px] [&>u]:underline-offset-[5px] md:[&>u]:decoration-[5px] 
+                  md:[&>u]:underline-offset-[10px] max-w-[1300px]"
+            >
+            Incepi o afacere de la 0?{' '}
+            </h2>
+            <span>
+              Te pot ajuta cu servicii complete de &nbsp;
+              <Link className="link" href={'/servicii/branding'}>
+                branding si identitate vizuala
+              </Link>
+            </span>
+          </div>
+        </HeaderWithParalax>
+      </section>
     </PageComponent>
   );
 };
