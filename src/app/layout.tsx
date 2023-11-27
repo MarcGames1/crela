ddf8dfc6a0e322f82b60cc1fe16fae1cbc89afdb
MarcGames1 <George.marcu20@gmail.com>
2023-11-28 00:03:09 +0200

    x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,28 +23,28 @@ export default function RootLayout({
     <html data-theme="crelaTheme" lang="ro">
       <head>
         {/* <!-- Google Tag Manager --> */}
-        <Script id='GTM'
+        {/* <Script id='GTM'
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            __html: `<script defer async>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PTTLWWC');</script>`,
           }}
         ></Script>
-        {/* <!-- End Google Tag Manager --> */}
+        <!-- End Google Tag Manager --> */}
       </head>
       <body className={`${font.className} grid grid-flow-row`}>
         {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
+        {/* <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PTTLWWC"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
-        </noscript>
+        </noscript> */}
         {/* <!-- End Google Tag Manager (noscript) --> */}
         {children}
       </body>
