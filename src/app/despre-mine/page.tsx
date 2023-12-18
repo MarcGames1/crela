97daedbaa1e6f8074e3e3a428b8461015d4488ce
MarcGames1 <George.marcu20@gmail.com>
2023-12-18 13:10:45 +0200

    update text

--- src/app/despre-mine/page.tsx
+++ src/app/despre-mine/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { PageComponent } from '@/components';
 const DespreMine = () => {
   return (
-    <PageComponent>Despre Mine</PageComponent>
+    <PageComponent>
+
+    </PageComponent>
   )
 }
 
