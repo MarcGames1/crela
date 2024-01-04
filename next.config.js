26820f51cd5632ec87801a9f4a469fe174c37461
MarcGames1 <George.marcu20@gmail.com>
2024-01-04 14:45:45 +0200

    xx

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')({
+disable: process.env.NODE_ENV === 'development',
   dest: 'public',
   register: true,
-  scope: '/',
   reloadOnOnline: true,
 cacheOnFrontEndNav: true,
-disable: false,
 
 
 });
