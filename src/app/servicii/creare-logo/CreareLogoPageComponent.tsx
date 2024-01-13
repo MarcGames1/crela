ba6cd9da2031cce797d29b625cde9380f5cbfae7
MarcGames1 <George.marcu20@gmail.com>
2024-01-13 21:44:08 +0200

    added dynamic import

---
+++ src/app/servicii/creare-logo/CreareLogoPageComponent.tsx
@@ -0,0 +1,69 @@
+import dynamic from 'next/dynamic';
+
+import React from 'react';
+import Link from 'next/link';
+import {
+  LogoDefinitionData,
+  LogoTransportSectionData,
+  LogoConstructionSectionData,
+} from './pageConstants';
+
+import { pageMetaData, MainHeaderData } from './pageConstants';
+
+type ComponentImport = () => Promise<any>;
+const DynamicComponents: Record<string, any> = {};
+
+const componentsToDynamicImport: Record<string, ComponentImport> = {
+  MainHeader: () => import('@/components/MainHeader/MainHeader'),
+  LogoGallery: () => import('./Components/LogoGallery'),
+  LogouriRealizate: () => import('./Components/LogouriRealizate'),
+  HeaderWithParalax: () =>
+    import('@/components').then((c) => c.HeaderWithParalax),
+  LogoDefinition: () => import('./Components/LogoDefinition'),
+};
+
+Object.keys(componentsToDynamicImport).forEach((componentName) => {
+  DynamicComponents[componentName] = dynamic(
+    componentsToDynamicImport[componentName]
+  );
+});
+
+
+
+const CreareLogoPageComponent = () => {
+  return (
+    <>
+      <DynamicComponents.MainHeader {...MainHeaderData} />
+
+      <DynamicComponents.LogoDefinition {...LogoDefinitionData} />
+      {/* <LogouriRealizate /> */}
+      <DynamicComponents.LogoGallery {...LogoTransportSectionData} />
+      <DynamicComponents.LogoGallery
+        rtl={true}
+        {...LogoConstructionSectionData}
+      />
+
+      <section className="flex flex-col">
+        <DynamicComponents.HeaderWithParalax bgImage="/images/assets/parallax1.webp">
+          <div className="m-auto">
+            <h2
+              className="mx-auto text-3xl font-bold leading-[1.25] md:text-5xl md:leading-tight 
+              [&>u]:decoration-[2px] [&>u]:underline-offset-[5px] md:[&>u]:decoration-[5px] 
+              md:[&>u]:underline-offset-[10px] max-w-[1300px]"
+            >
+              Incepi o afacere de la 0?{' '}
+            </h2>
+            <span>
+              Te pot ajuta cu servicii complete de &nbsp;
+              <Link className="link" href={'/servicii/branding'}>
+                branding si identitate vizuala
+              </Link>
+            </span>
+          </div>
+        </DynamicComponents.HeaderWithParalax>
+      </section>
+    </>
+  );
+};
+
+export default CreareLogoPageComponent;
--- src/app/servicii/creare-logo/page.tsx
+++ src/app/servicii/creare-logo/page.tsx
@@ -1,80 +1,11 @@
 import dynamic from 'next/dynamic';
 
-import React from 'react';
-import Link from 'next/link';
-import { LogoDefinitionData, LogoTransportSectionData, LogoConstructionSectionData } from './pageConstants';
 import { Metadata } from 'next/types';
-import { pageMetaData, MainHeaderData } from './pageConstants';
+import { pageMetaData } from './pageConstants';
+import CreareLogoPageComponent from './CreareLogoPageComponent';
 
+export const metadata: Metadata = pageMetaData;
 
-type ComponentImport = () => Promise<any>;
-const DynamicComponents: Record<string, any> = {};
-
-
-
-
-const componentsToDynamicImport: Record<string, ComponentImport> = {
-  MainHeader: () => import('@/components/MainHeader/MainHeader'),
-  LogoGallery: () => import('./Components/LogoGallery'),
-  LogouriRealizate: () => import('./Components/LogouriRealizate'),
-  HeaderWithParalax: () =>
-    import('@/components').then((c) => c.HeaderWithParalax),
-  LogoDefinition: () =>
-    import('./Components/LogoDefinition'),
-};
-
-
-Object.keys(componentsToDynamicImport).forEach((componentName) => {
-  DynamicComponents[componentName] = dynamic(
-    componentsToDynamicImport[componentName]
-  );
-});
-
-
-export const metadata: Metadata = pageMetaData
-
-const CreareLogo = () => {
-  return (
-    <>
-      <DynamicComponents.MainHeader {...MainHeaderData} />
-
-      <DynamicComponents.LogoDefinition {...LogoDefinitionData} />
-      {/* <LogouriRealizate /> */}
-      <DynamicComponents.LogoGallery {...LogoTransportSectionData} />
-      <DynamicComponents.LogoGallery
-        rtl={true}
-        {...LogoConstructionSectionData}
-      />
-
-      <section className="flex flex-col">
-        <DynamicComponents.HeaderWithParalax bgImage="/images/assets/parallax1.webp">
-          <div className="m-auto">
-            <h2
-              className="mx-auto text-3xl font-bold leading-[1.25] md:text-5xl md:leading-tight 
-              [&>u]:decoration-[2px] [&>u]:underline-offset-[5px] md:[&>u]:decoration-[5px] 
-              md:[&>u]:underline-offset-[10px] max-w-[1300px]"
-            >
-              Incepi o afacere de la 0?{' '}
-            </h2>
-            <span>
-              Te pot ajuta cu servicii complete de &nbsp;
-              <Link className="link" href={'/servicii/branding'}>
-                branding si identitate vizuala
-              </Link>
-            </span>
-          </div>
-        </DynamicComponents.HeaderWithParalax>
-      </section>
-    </>
-  );
-};
-
-
+const CreareLogo = CreareLogoPageComponent;
 
 export default CreareLogo;
-
-
-
-
-
-
