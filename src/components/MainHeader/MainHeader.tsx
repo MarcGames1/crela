b58a27339cad710d6ce975be71329af31aa65dbf
MarcGames1 <George.marcu20@gmail.com>
2024-01-26 00:02:04 +0200

    Update MainHeader.tsx

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -3,9 +3,9 @@ import { ActionBtn } from "../Buttons";
 import { IMainHeader } from "@/interfaces/IMainHeader";
 
 const MainHeader = ({ title, paragraph, image, video }: IMainHeader) => (
-  <section className="p-10 max-w-full relative overflow-hidden pt-10 pb-2 md:py-24">
+  <section className="p-10 max-w-full relative overflow-hidden pt-10 pb-2 flex justify-evenly md:py-24">
     <div className="absolute bottom-[0px] left-0 h-full w-[200%]   overflow-x-hidden origin-bottom-left -translate-y-4/12 -rotate-12 transform bg-primary-gradient md:translate-y-0"></div>
-    <div className="container relative grid min-h-[50vh]  lg:grid-flow-col gap-5 overflow-x-hidden  md:grid-flow-row md:gap-12">
+    <div className="container relative grid min-h-[50vh]  lg:grid-flow-col gap-5 overflow-x-hidden  md:grid-flow-row md:gap-12 ">
       <div className="lg:p-10 flex-1 w-full text-white ">
         <h1 className="text-3xl font-bold uppercase leading-[1.2] tracking-wider md:text-5xl">
           {title}
