81f114586ba3925faa3de0e78bedc85c42157f79
Marcu Alexandru <George.marcu20@gmail.com>
2024-02-07 11:27:12 -0800

    Update SingleVideo.tsx

--- src/app/servicii/creare-continut/SingleVideo.tsx
+++ src/app/servicii/creare-continut/SingleVideo.tsx
@@ -1,25 +1,30 @@
 import { PhoneMockup } from '@/components';
 import React from 'react'
 import { UGC_Video } from './UGC_Data';
+import dynamic from 'next/dynamic';
 
 const SingleVideo = ({src, header, description}:UGC_Video) => {
   return (
-    <div id='phoneContainer'  className="py-5 min-w-[500px] w-auto flex items-center flex-col gap-2  mx-auto">
+    <div
+      id="phoneContainer"
+      className="py-5 min-w-[500px] w-auto flex items-center flex-col gap-2  mx-auto"
+    >
       <PhoneMockup>
         <div>
-          <video loop muted controls>
+          <video preload="none" loop muted controls>
             <source src={src} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
       </PhoneMockup>
-      <div className='y'>
-      <p className='text-center text-sm'>{header}</p>
-      <p className='text-center text-lg font-semibold'>{description}</p>
-
+      <div className="y">
+        <p className="text-center text-sm">{header}</p>
+        <p className="text-center text-lg font-semibold">{description}</p>
       </div>
     </div>
   );
 }
 
-export default SingleVideo
+export default dynamic(() => Promise.resolve(SingleVideo), {
+  ssr: true,
+});
