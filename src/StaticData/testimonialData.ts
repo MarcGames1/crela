ab6955ddbda71008b6855735d61cb9acb6cc1aa9
MarcGames1 <George.marcu20@gmail.com>
2024-02-20 20:23:59 +0200

    updated  testimonials

--- src/StaticData/testimonialData.ts
+++ src/StaticData/testimonialData.ts
@@ -3,10 +3,24 @@ import { TestimonialData } from "@/components/TestimonialsCarousel/testimonialDa
 const testimonialsLocation = '/images/testimoniale';
 export const testimonialData: TestimonialData[] = [
   {
+    name: 'Razvan Paul Prichici - Fondator Get around digital',
+    paragraph:
+      'Colaborarea cu Laura a fost o experiență fantastică! A creat un logo care surprinde perfect esența brandului meu, iar cele patru ilustrații grafice captivante pentru cubul rotitor de pe site-ul meu nu sunt doar vizual impresionante, ci sunt și integrate perfect în designul general. Laura a înțeles viziunea mea, a livrat la timp și a fost receptivă la feedback. Recomand cu încredere serviciile Laurei pentru oricine caută un design de înaltă calitate. Mulțumesc pentru o colaborare excelentă!',
+    header: 'Colaborarea cu Laura a fost o experiență fantastică!',
+    picture: testimonialsLocation + '/Think-Digital-Logo.png',
+  },
+  {
+    name: 'Exclusive Print',
+    paragraph:
+      'Colaborez cu Crela de peste un an  si pot spune ca ofera servicii de creare continut excelente! Articolele scrise m-au ajutat si mi-au adus trafic atat online cat si fizic in businessul pe care il detin!. Recomand cu incredere',
+    header: 'Creator de continut extraordinar!',
+    picture: testimonialsLocation + '/EXCLUSIVE-PRINT-logo.png',
+  },
+  {
     name: 'Andreea Voicu Aliand Consulting',
     paragraph:
       'Laura este un designer talentat, cu o simțire cromatică și estetică impresionantă. Reușește să mă surprindă plăcut de fiecare dată când avem un proiect comun și mă bazez în întregime pe expertiza ei în această zonă. Este harnică, respectă cu strictețe termenele limită și are o creativitate copleșitoare uneori. O recomand cu căldură!',
-    header: 'Designer talentat cu o simț cromatic și estetic impresionant',
+    header: 'Designer talentat cu un simț cromatic și estetic impresionant',
     picture: testimonialsLocation + '/andreea-voicu.png',
   },
   {
