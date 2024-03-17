b8075ce6c0ecdd35e71b5db1611f3e18318db017
MarcGames1 <George.marcu20@gmail.com>
2024-03-17 16:04:21 +0200

    Update TiktokVideoContainer.tsx

--- src/app/servicii/creare-continut/Components/TiktokVideoContainer.tsx
+++ src/app/servicii/creare-continut/Components/TiktokVideoContainer.tsx
@@ -1,12 +1,41 @@
+import dynamic from "next/dynamic";
+
 const TiktokVideoContainer = () => {
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@theskincarerecipe/video/7339236948743032096" data-video-id="7339236948743032096" data-embed-from="embed_page"> <section> <a target="_blank" title="@theskincarerecipe" href="https://www.tiktok.com/@theskincarerecipe?refer=embed">@theskincarerecipe</a> <p>Produsele chiar m-au ajutat foarte mult cu caderea parului, dar si cu regenerarea acestuia. Urmeaza si partea a doua😊 Sper sa va ajute🫶🏻</p> <a target="_blank" title="♬ som original - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔 - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔" href="https://www.tiktok.com/music/som-original-𝒆𝒅𝒖𝒆𝒗𝒍'𝒔-7300714218735192837?refer=embed">♬ som original - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔 - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>`,
-      }}
-      id={"tiktokVideoContainer"}
-    ></div>
+    <div id="tiktokVideoContainer">
+      <blockquote
+        className="tiktok-embed"
+        cite="https://www.tiktok.com/@theskincarerecipe/video/7339236948743032096"
+        data-video-id="7339236948743032096"
+        data-embed-from="embed_page"
+      >
+        <section>
+          <a
+            target="_blank"
+            title="@theskincarerecipe"
+            href="https://www.tiktok.com/@theskincarerecipe?refer=embed"
+          >
+            @theskincarerecipe
+          </a>
+          <p>
+            Produsele chiar m-au ajutat foarte mult cu caderea parului, dar si
+            cu regenerarea acestuia. Urmeaza si partea a doua😊 Sper sa va
+            ajute🫶🏻
+          </p>
+          <a
+            target="_blank"
+            title="♬ som original - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔 - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔"
+            href="https://www.tiktok.com/music/som-original-𝒆𝒅𝒖𝒆𝒗𝒍'𝒔-7300714218735192837?refer=embed"
+          >
+            ♬ som original - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔 - 𝒆𝒅𝒖𝒆𝒗𝒍'𝒔
+          </a>
+        </section>
+      </blockquote>
+      <script async src="https://www.tiktok.com/embed.js"></script>
+    </div>
   );
 };
 
-export default TiktokVideoContainer;
+export default dynamic(() => Promise.resolve(TiktokVideoContainer), {
+  ssr: false,
+});
