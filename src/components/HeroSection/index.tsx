422c9c04990aa9326248f7d6a8adae1ebacada1f
MarcGames1 <George.marcu20@gmail.com>
2024-03-28 18:45:25 +0200

    Update index.tsx

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -17,20 +17,29 @@ const HeroSection = () => {
   return (
     <section>
       <div className={"prose m-auto block"}>
-        <div className={"lg:text-3xl p-5 md:text-2xl text-center font-bold"}>
+        <div
+          className={
+            "lg:text-3xl p-5 md:text-2xl text-center font-bold k bg-clip-text  text-transparent bg-primary-gradient"
+          }
+        >
           <TypewriterEffect strings={strings} />
         </div>
 
         <p className={"lg:text-3xl p-5 md:text-2xl text-center font-bold"}>
           Ajutăm brandurile să facă diferența.
         </p>
-        <div className={"lg:text-2xl p-5 md:text-xl text-center font-semibold"}>
+        <div className={"lg:text-2xl p-5 md:text-xl text-center font-bold"}>
           <p>Oferim soluții personalizate</p>
           <p> pentru a te ajuta să devii</p>
           <p> memorabil în mediul online.</p>
         </div>
         <div className={"lg:text-3xl p-5 md:text-2xl text-center font-bold"}>
-          Ai o idee? Hai s-o transformăm în gigantul de mâine!
+          Ai o idee?{" "}
+          <span
+            className={"bg-clip-text  text-transparent bg-primary-gradient"}
+          >
+            Hai s-o transformăm în gigantul de mâine!
+          </span>
         </div>
       </div>
     </section>
