82d118fc1d64507e5f2b57da01003e10ef845848
Marcu Alexandru <George.marcu20@gmail.com>
2025-01-06 08:58:16 +0200

    removed calendly dependency

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -21,8 +21,8 @@ const Whatsapp = ({
 const MainCta = ({
   className = " btn hover:text-[#DA2128] ml-[7px] hover:ml-[2px]  hover:border-[#DA2128] hover:border-[3px] hover:bg-transparent hover:bg-none hover:border-opacity-100	 bg-primary-gradient border-[0px] border-solid border-white border-opacity-0 text-white transition-colors duration-300",
   text = "Consultanta Gratuita",
-  dataTip = "Programeaza-te pentru o sedinta gratuita pe Zoom!",
-  href = "https://calendly.com/crela/consultanta-gratuita",
+  dataTip = "Vrei să programăm o ședință? Dă-mi un mesaj pe WhatsApp!",
+  href = "https://wa.me/40766713369?text=Bun%C4%83%2C%20a%C8%99%20dori%20s%C4%83%20programez%20o%20consulta%C8%9Bie%20gratuit%C4%83%20pentru",
 }) => {
   return (
     <Link title={dataTip} className={className} href={href}>
@@ -34,7 +34,8 @@ const MainCta = ({
 const Primary = () => {
   return (
     <a
-      href={"https://calendly.com/crela/consultanta-gratuita"}
+        title={'Vrei să programăm o ședință? Dă-mi un mesaj pe WhatsApp!'}
+      href={"https://wa.me/40766713369?text=Bun%C4%83%2C%20a%C8%99%20dori%20s%C4%83%20programez%20o%20consulta%C8%9Bie%20gratuit%C4%83%20pentru"}
       className={` btn btn-secondary hover:text-secondary ml-[7px] hover:ml-[2px]  hover:border-secondary hover:border-[3px] hover:bg-transparent hover:bg-none hover:border-opacity-100	 border-[0px] border-solid border-white border-opacity-0 text-white transition-colors duration-300', ${styles.secondary}`}
     >
       <span className={`${styles.link}`}>Contacteaza-ma</span>
